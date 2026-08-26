80c1be7a088c03fd3dcfdc1340de60ff92904a9a
kelvin-mutugi <kelvinmutugi925@gmail.com>
2026-08-26 19:30:18 +0300

    project setup

--- main.cpp
+++ main.cpp
@@ -6,24 +6,19 @@ int screen_width = 1800;
 int screen_height = 900;
 
 int main(){
-
-    InitWindow(screen_width, screen_height, "my raylib screen");
+    InitWindow(screen_width, screen_height, "NUCLEAR EXPLOSION SIMULATION");
     SetTargetFPS(30);
 
     while(!WindowShouldClose()){
         BeginDrawing();
 
         ClearBackground(BLACK);
-        DrawText("Congrats! You created a window!", 190, 200, 20, BLACK);
-
-        DrawPixel(800,450, RED);   
-        DrawLine(0, 850, 1600, 850, RED);   
+  
         
         EndDrawing();
     }
 
+    
     CloseWindow();
-
-
     return 0;
 }
